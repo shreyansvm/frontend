5a92fab69649ac016e31957baa409a6e50d39ca4
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-07-08 16:04:41 -0700

    trying new things in dash_html_components

--- myFavStocks.py
+++ myFavStocks.py
@@ -9,17 +9,43 @@ import datetime
 app = dash.Dash()
 
 app.layout = html.Div([
-    html.H1('Stock Tickers'),
+    html.H1('BytePython Ventures', className='banner'),
+    html.Div([
+        html.P('Welcome to BytePython Ventures - Investments'),
+        html.P('Currently our investments range from Tech to Pharma to Energy.')
+    ]),
+
+    ### TODO : This functionality not yet working. Handle how user-selected start and end time can be passed to callback method
+    html.Div([
+        html.P("Start-Time"),
+        dcc.Dropdown(id='start_time', options=[
+                     {'label': i, 'value': i} for i in ['Jan 1st 2016', 'Jan 1st 2017', 'Jan 1st 2018']], value='Jan 1st 2018')
+    ], style={'width': '40%', 'margin-left': '3%', 'display': 'inline-block'}),
+
+    html.Div([
+        html.P("End-Time"),
+        dcc.Dropdown(id='end_time', options=[
+                     {'label': i, 'value': i} for i in ['Jan 1st 2016', 'Jan 1st 2017', 'Jan 1st 2018']], value='Jan 1st 2018')
+    ], style={'width': '31%', 'margin-left': '3%', 'display': 'inline-block'}),
+
+    html.Div([
+        html.P("Please select the stock from the drop-down to view its current stock price between the above start and end dates")
+    ]),
+    ######################
+
     dcc.Dropdown(
         id='my-dropdown',
         options=[
             {'label': 'Nvidia', 'value': 'NVDA'},
             {'label': 'Apple', 'value': 'AAPL'},
-            {'label': 'Twitter', 'value': 'TWTR'},
+            {'label': 'AbbVie', 'value': 'ABBV'},
             {'label': 'Facebook', 'value': 'FB'},
-            {'label': 'Shopify', 'value': 'SHOP'}
+            {'label': 'Shopify', 'value': 'SHOP'},
+            {'label': 'Twitter', 'value': 'TWTR'},
+            {'label': 'JPMorgan Chase & Co', 'value': 'JPM'},
+            {'label': 'Exxon Mobil Corp', 'value': 'XOM'},
         ],
-        value='COKE'
+        value='NVDA'
     ),
     dcc.Graph(id='my-graph')
 ])
@@ -37,4 +63,4 @@ def update_graph(selected_dropdown_value):
     }
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(host='127.0.0.1', port=8888)
